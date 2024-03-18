74277c6e725b9d65921605f6029dc2450351a569
Mohamed Essebaiy <ahmadessebaiy@gmail.com>
2024-03-18 16:17:14 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(18, 55, 42,0)),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
