3a55563450b4fe28e73206d6ab95ccceb7fc6c44
Mohamed Essebaiy <ahmadessebaiy@gmail.com>
2024-03-19 17:54:11 +0000

    Create Grid View with class Item of grid and data of categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meal_app/screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.dark,
             seedColor: const Color.fromRGBO(18, 55, 42,0)),
         useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: const Scaffold(),
+      home: CategoriesScreen(),
     );
   }
 }
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal App'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 3/2),
+      children: [
+        for(final category in availableCategories)
+          CategoryGridItem(category: category)
+      ],),
+    );
+  }
+}
