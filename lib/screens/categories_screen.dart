67a8a4966d004095a886b9e5fee02901390ad7ae
Mohamed Essebaiy <ahmadessebaiy@gmail.com>
2024-03-20 02:07:18 +0000

    Add Inkwell to Item of grid

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,7 +9,7 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Meal App'),
+        title: const Text('Pick your Category'),
       ),
       body: GridView(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal'),
+      ),
+      body: const Text(''),
+    );
+  }
+}
