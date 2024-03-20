4b43b21b5c8958983012450101fec8983c982067
Mohamed Essebaiy <ahmadessebaiy@gmail.com>
2024-03-20 16:07:35 +0000

    Create a Positioned widget on Meal Item class

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
 import 'package:transparent_image/transparent_image.dart';
@@ -10,13 +11,41 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8)
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 20,
       child: InkWell(
         child: Stack(
           children: [
             FadeInImage(
                 placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(meal.imageUrl)),
-            Text(meal.title)
+                image: NetworkImage(meal.imageUrl)
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 40),
+                  color: Colors.black38,
+                  child: Column(
+                    children: [
+                      Text(meal.title,
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),),
+                    ],
+                  )),
+            )
           ],
         ),
     ),
